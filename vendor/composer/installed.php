b2ba77e907295467e3960b92ea584ec282e3f69d
Sivasamy <sivasamy.t@gmail.com>
2024-11-20 16:50:04 +0530

    Update installed.php after Composer install

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'codeigniter4/framework',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '425c9b67ee55a1dfc9e35a85bd0da436c6c50719',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -29,9 +29,9 @@
             'dev_requirement' => true,
         ),
         'codeigniter4/framework' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '425c9b67ee55a1dfc9e35a85bd0da436c6c50719',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
